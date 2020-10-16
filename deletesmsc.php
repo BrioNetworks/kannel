e49ce42039abc251cc3a24c9744b077c8d14ef1d
anildalar <dollorinfotech@gmail.com>
2020-10-16 13:41:18 +0530

    This is my second commit

--- deletesmsc.php
+++ deletesmsc.php
@@ -1,4 +1,53 @@
 <?php 
-	echo 'Hello Its a test';
+	if( !empty($_GET['smsc'])){
+		echo $_GET['smsc'];
+	}
+	
+	
+	
+	
 
 ?>
+<?php 
+/*
+group=smsc
+smsc=smpp
+smsc-id=DOLLOR1
+host=
+port=
+smsc-username=
+smsc-password=
+transceiver-mode=true
+system-type=
+service-type=
+interface-version=
+throughput=
+address-range=
+source-addr-ton=
+source-addr-npi=
+dest-addr-ton=
+dest-addr-npi=
+allowed-smsc-id=
+enquire-link-interval=
+ssl-client-certkey-file=
+use-ssl=
+my-number=
+receive-port=
+max-pending-submits=
+reconnect-delay=
+source-addr-autodetect=
+bind-addr-ton=
+bind-addr-npi=
+msg-id-type=
+alt-charset=
+alt-addr-charset=
+connection-timeout=0
+wait-ack=
+wait-ack-expire=
+validityperiod=
+esm-class=
+log-format=
+
+
+
+*/
